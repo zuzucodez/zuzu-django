27500468bd97c1a873efb98e721ac852395695ae
zuzucodez <zuzucodez@gmail.com>
2021-08-03 15:22:04 -0400

    this is my first commit

---
+++ store/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from category.models import Category
+
+# Create your views here.
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories =  get_object_or_404(Category, slug=category_slug)
+        products =  Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products =  Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+
+    return render(request, 'store/store.html', context)
+
+    
+# metodo para ir  al la pagina de un solo producto
+def product_detail(request, category_slug, product_slug):
+    # ahora  vamos a sacar la info de cada item o product_count
+    try:
+        single_product  = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context =  {
+        'single_product': single_product
+    }
+    return render(request, 'store/product_detail.html', context)
