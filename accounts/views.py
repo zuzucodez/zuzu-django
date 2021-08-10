bfe250a64e4c7337a9b59321f9d69903748af3b7
zuzucodez <zuzucodez@gmail.com>
2021-08-10 14:34:26 -0400

    Solving some bugs in loggedin user carts

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,10 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens  import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
+
 # Create your views here.
 def register(request):
     # validando datos del form
@@ -64,9 +68,63 @@ def login(request):
         user=auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart=Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # pasos para hacer que los item si tienen la mismo variaciones se agrupen y no esten individual
+                    #existing variations -> database
+                    #current variation ->  product_variation
+                    # item_id ->database
+
+                    #obteniendo product_variation por cart_id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #obteniendo el cartitem  de el usuario para acceder a product_variation
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3,  4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index  = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.save()
+                        else:
+                            cart_item  =  CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
+
             auth.login(request, user)
             messages.success(request, 'You are logged in')
-            return redirect('dashboard')
+            #aki vamos a usar la libreria requests para  hacer cambios en  el url cuando queremos que se redirija a otra url
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect('checkout')
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials!')
             return redirect('login')
@@ -160,7 +218,7 @@ def resetpassword(request):
             user = Account.objects.get(pk=uid)
             user.set_password(password)
             user.save()
-            
+
             messages.success(request, 'Congratulations, you have changed your password!!')
             return redirect('login')
         else:
